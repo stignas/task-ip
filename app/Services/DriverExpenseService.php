7d676f5af2bcae4ee71f0d8373d6f93160b8d73a
Ignas <statkus.online@gmail.com>
2023-02-16 15:19:49 +0200

    driver expense service logic created

--- app/Services/DriverExpenseService.php
+++ app/Services/DriverExpenseService.php
@@ -7,8 +7,33 @@ class DriverExpenseService
     public function calculateDriverExpenses(array $drivers, array $expenses)
     {
         $result = [];
+        $counter = 0;
+        $result['Expenses'] = $expenses;
+        $result['Expenses']['total'] = number_format(array_sum($result['Expenses']), 2, '.','');
 
+        foreach ($expenses as $expenseType => $expenseAmount) {
+            $halfExpense = $expenseAmount / 2;
+            $halfExpenseRoundUp = number_format(round($halfExpense, 2), 2, '.', '');
+            $halfExpenseRoundDown = number_format(round($halfExpense, 2, PHP_ROUND_HALF_DOWN), 2, '.', '');
 
+            if ($halfExpense != number_format($halfExpense, 2)) {
+                $counter++;
+                if ($counter % 2 != 0) {
+                    $result[$drivers[0]][$expenseType] = $halfExpenseRoundUp;
+                    $result[$drivers[1]][$expenseType] = $halfExpenseRoundDown;
+                } else {
+                    $result[$drivers[0]][$expenseType] = $halfExpenseRoundDown;
+                    $result[$drivers[1]][$expenseType] = $halfExpenseRoundUp;
+                }
+            } else {
+                $result[$drivers[0]][$expenseType] = number_format($halfExpense, 2, '.', '');
+                $result[$drivers[1]][$expenseType] = number_format($halfExpense, 2, '.', '');
+            }
+        }
+
+        // Totals
+        $result[$drivers[0]]['total'] = number_format(array_sum($result[$drivers[0]]), 2,'.','');
+        $result[$drivers[1]]['total'] = number_format(array_sum($result[$drivers[1]]), 2,'.','');
         return $result;
     }
 }
