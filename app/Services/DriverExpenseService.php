30471b1f7c5ba7f6d2b7037fee37f8d4312c5fee
Ignas <statkus.online@gmail.com>
2023-02-16 14:47:56 +0200

    initial project setup

---
+++ app/Services/DriverExpenseService.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Services;
+
+class DriverExpenseService
+{
+    public function calculateDriverExpenses(array $drivers, array $expenses)
+    {
+        $result = [];
+
+
+        return $result;
+    }
+}
